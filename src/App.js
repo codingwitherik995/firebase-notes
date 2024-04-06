88ec10eff6018520a9953a78f22a67c0244b7e5b
Miklodi Erik <miklodi.erik@gmail.com>
2024-04-06 10:38:38 +0200

    basic routing implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { auth, googleProvider } from "./firebase";
 import { signInWithPopup } from "firebase/auth";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <button>Sign in with google</button>
+          </li>
+          <li>
+            <a href="/notes">notes</a>
+          </li>
+          <li>
+            <a href="/profile">
+              <img src="" alt="Profile" />
+            </a>
+          </li>
+          <li>
+            <button>Sign out</button>
+          </li>
+        </ul>
+      </nav>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element="login" />
+          <Route path="/profile" element="profile" />
+          <Route path="/notes" element="notes" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
