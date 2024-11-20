549bc162686ffe193e8bdbd650d72b0dbd4e7457
codingwitherik995 <codingwitherik995@gmail.com>
2024-11-20 10:56:16 +0100

    extend the cicd pipeline with unit test

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Login from "./pages/Login";
+
+describe("Login Page", () => {
+  it("renders the 'Login' title", () => {
+    // Render the Login component
+    render(<Login />);
+
+    // Check if the "Login" title is in the document
+    const loginTitle = screen.getByText(/Login/i);
+    expect(loginTitle).toBeInTheDocument();
+  });
+});
