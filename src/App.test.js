1b36b64e91e9b7e38d3e91e49edd4dd4d81e1e66
codingwitherik995 <codingwitherik995@gmail.com>
2024-11-20 11:02:38 +0100

    separate unit test to a different job

--- src/App.test.js
+++ src/App.test.js
@@ -9,7 +9,7 @@ describe("Login Page", () => {
     render(<Login />);
 
     // Check if the "Login" title is in the document
-    const loginTitle = screen.getByText(/Logins/i);
+    const loginTitle = screen.getByText(/Login/i);
     expect(loginTitle).toBeInTheDocument();
   });
 });
